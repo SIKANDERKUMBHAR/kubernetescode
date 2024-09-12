c84636fa01491a58ad4897df6ff1f432a4cd2380
Sikander Ali <112273879+SIKANDERKUMBHAR@users.noreply.github.com>
2024-09-13 00:04:22 +0500

    added

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, !!'
+    return 'Please subscribe, This is me Sikander Ali deployed with Argocd(cd) and jenkins(ci)!!'
